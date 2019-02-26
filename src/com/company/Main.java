0bb49ec75bdc6560ea3440f08cc9b3fe59c6412a
Anna.Allen <anna.allen@AnnaAllens-MacBook-Pro.local>
2019-02-26 17:27:25 +0000

    Array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner1 = new Scanner(System.in);
-        int first = scanner1.nextInt();
-        int second = scanner1.nextInt();
-
-
-        System.out.println(first);
-        System.out.println(second);
 
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter the length: ");
+        int length = scanner .nextInt();
+        int[] inputNumbers = new int[length];
+        for (int i=0; i<=length-1; i++) {
+            System.out.print("Please enter a number: ");
+            inputNumbers[i] = scanner .nextInt();
+        }
+
+        System.out.print("Please enter an operation: ");
         String operation = scanner.next();
 
         Cancalculate calculate;
@@ -30,7 +32,12 @@ public class Main {
             return;
         }
 
-        System.out.println(calculate.calculate(first,second));
+        int answer = inputNumbers[0];
+        for (int i=1; i<=length-1; i++){
+            answer = calculate.calculate(inputNumbers[i],answer);
+        }
+
+        System.out.println(answer);
     }
 }
 
